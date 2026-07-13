156dce3472a0fdab328bbea83d382fa0d882095d
nathavinicius0202 <nathavinicius0202@gmail.com>
2026-07-12 23:20:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,32 @@ app = FastAPI(
     version="1.0.0"
 )
 
+# Banco temporário em memória
+produtos = []
+
 @app.get("/")
 def inicio():
-    return {
-        "mensagem": "Manto Sagrado funcionando!"
-    }
+    return {"mensagem": "Manto Sagrado funcionando!"}
 
 @app.get("/status")
 def status():
     return {
         "status": "online",
         "ultima_atualizacao": datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
-        "versao": "1.0.0"
+        "produtos": len(produtos)
     }
+
+@app.get("/produtos")
+def listar_produtos():
+    return produtos
+
+@app.post("/produto-teste")
+def criar_produto():
+    produto = {
+        "id": 1,
+        "nome": "Camisa Palmeiras I 2026/27",
+        "preco": 149.90,
+        "categoria": "Brasileirão"
+    }
+    produtos.append(produto)
+    return {"mensagem": "Produto adicionado", "produto": produto}
